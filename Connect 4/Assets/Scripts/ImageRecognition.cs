def127dae45a0f8643140f009bab687e22d5b369
ichimgabi97 <ichimgabi97@gmail.com>
2021-12-17 09:03:27 +0200

    AR

---
+++ Connect 4/Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+
+    private ARTrackedImageManager _arTrackedImageManager;
+
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name); 
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
